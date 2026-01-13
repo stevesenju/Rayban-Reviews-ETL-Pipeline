1aa6ff968e384039d825259485b166e1813bc9e4
stevesenju <134808424+stevesenju@users.noreply.github.com>
2026-01-12 22:49:36 -0500

    Project Structure
    Directory change

--- ETL-Rayban-Reviews/etl/extract.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def extract_reviews(csv_path: str) -> pd.DataFrame:
-    df = pd.read_csv(csv_path)
-    return df
--- ETL-Rayban-Reviews/etl/load.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def load_to_csv(df: pd.DataFrame, output_path: str):
-    df.to_csv(output_path, index=False)
